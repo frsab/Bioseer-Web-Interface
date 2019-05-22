5fd4055949b800bb119c67f6b03460655deef1e3
Travis CI <github@travis-ci.org>
2019-05-21 17:29:52 -0700

    trying to fix this script

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ git config --global user.name "Travis CI"
 git config --global user.email "github@travis-ci.org"
 
 git add .
-git commit -a "Deploy to GitHub Pages"
+git commit -a -m "Deploy to GitHub Pages"
 
 git push --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
