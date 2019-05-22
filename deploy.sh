d79da9fd0970cdbbc0a6451e19435c6878085f74
Travis CI <github@travis-ci.org>
2019-05-21 17:27:39 -0700

    trying to fix this script

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,12 @@
 GIT_DEPLOY_REPO=${GIT_DEPLOY_REPO:-$(node -p -e "require('./package.json').repository.url")}
 
 sudo rm -R .git
-git init && \
+git init
 
-git config --global user.name "Travis CI" && \
-git config --global user.email "github@travis-ci.org" && \
+git config --global user.name "Travis CI"
+git config --global user.email "github@travis-ci.org"
 
-git add . && \
-git commit -m "Deploy to GitHub Pages" && \
+git add .
+git commit -a "Deploy to GitHub Pages"
 
 git push --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
