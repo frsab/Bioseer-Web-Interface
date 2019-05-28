3603fe8adb1ae3a4e52b42fea19bade8851aeed9
Travis CI <github@travis-ci.org>
2019-05-28 13:36:22 -0700

    Firebase ready, distributed map-overlay

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,18 @@ import {BingApiLoaderService} from './services/bing-api-loader.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MaterialModule} from './shared/material/material.module';
 import { MapOverlayComponent } from './map-overlay/map-overlay.component';
+import { OverviewComponent } from './map-overlay/overview/overview.component';
+import { GroupComponent } from './map-overlay/group/group.component';
+import { MapSettingsComponent } from './map-overlay/map-settings/map-settings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BingMapComponent,
-    MapOverlayComponent
+    MapOverlayComponent,
+    OverviewComponent,
+    GroupComponent,
+    MapSettingsComponent
   ],
   imports: [
     BrowserModule,
