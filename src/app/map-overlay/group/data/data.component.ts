28dfe4c19bee98a0941657f8dbadd573ff5eeff6
Travis CI <github@travis-ci.org>
2019-05-28 21:15:31 -0700

    Finished data tab

--- src/app/map-overlay/group/data/data.component.ts
+++ src/app/map-overlay/group/data/data.component.ts
@@ -16,7 +16,7 @@ export class DataComponent implements OnInit {
     private router: Router) {
     this.router.events.subscribe((e) => {
       if (e instanceof NavigationEnd) {
-        console.log('Na End')
+        console.log('Na End');
       }
     });
   }
@@ -25,25 +25,25 @@ export class DataComponent implements OnInit {
     this.LineChart = new Chart('lineChart', {
       type: 'line',
       data: {
-        labels: ["Jan", "Feb", "March", "April", "May", "June","July","Aug","Sep","Oct","Nov","Dec"],
+        labels: ['Jan', 'Feb', 'March', 'April', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
         datasets: [{
-          label: 'Number of Items Sold in Months',
-          data: [9,7 , 3, 5, 2, 10,15,16,19,3,1,9],
-          fill:false,
-          lineTension:0.2,
-          borderColor:"red",
+          label: 'Temperature in Celsius',
+          data: [9, 7 , 3, 5, 2, 10, 15, 16, 19, 3, 1, 9],
+          fill: false,
+          lineTension: 0.2,
+          borderColor: 'red',
           borderWidth: 1
         }]
       },
       options: {
-        title:{
-          text:"Line Chart",
-          display:true
+        title: {
+          text: '',
+          display: true
         },
         scales: {
           yAxes: [{
             ticks: {
-              beginAtZero:true
+              beginAtZero: true
             }
           }]
         }
