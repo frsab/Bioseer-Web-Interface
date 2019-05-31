321bd507408fdde34b1827288b1bb7e3dbe46e15
Travis CI <github@travis-ci.org>
2019-05-31 11:13:41 -0700

    Updated server

--- api/server.js
+++ api/server.js
@@ -5,6 +5,22 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const jwt = require('./helpers/jwt');
 const errorHandler = require('./helpers/error-handler');
+const path = require('path');
+const favicon = require('serve-favicon');
+
+const root = './';
+
+const allowedExt = [
+  '.js',
+  '.ico',
+  '.css',
+  '.png',
+  '.jpg',
+  '.woff2',
+  '.woff',
+  '.ttf',
+  '.svg',
+];
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -13,8 +29,15 @@ app.use(cors());
 // use JWT auth to secure the api
 app.use(jwt());
 
+// Favicon
+app.use(favicon(path.join(__dirname, './', 'favicon.ico')));
+
 // api routes
+app.use(express.static(path.join(root, 'dist/Bioseer-Web-Interface')));
 app.use('/users', require('./users/users.controller'));
+app.get('*', (req, res) => {
+  res.sendFile('../dist/Bioseer-Web-Interface/index.html', {root});
+});
 
 // global error handler
 app.use(errorHandler);
