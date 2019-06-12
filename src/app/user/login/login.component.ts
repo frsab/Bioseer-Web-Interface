32ba767fe7b6cd8f193b6e3eed734476b55290da
Caelin Sutch <sutchc20@student.jhs.net>
2019-06-12 13:34:41 -0700

    Fixed login form

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {ActivatedRoute, Router} from '@angular/router';
 import {AuthenticationService} from '../../_services/authentication.service';
 import {first} from 'rxjs/operators';
+import {User} from '../../_models/user.model';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -11,8 +13,9 @@ import {first} from 'rxjs/operators';
 })
 export class LoginComponent implements OnInit {
 
-  loginForm: FormGroup;
-  loading = false;
+  formGroup: FormGroup;
+  titleAlert = 'This field is required';
+  post: any = '';
   submitted = false;
   returnUrl: string;
   error = '';
@@ -22,34 +25,28 @@ export class LoginComponent implements OnInit {
     private route: ActivatedRoute,
     private router: Router,
     private authenticationService: AuthenticationService
-  ) { }
+  ) {
+  }
 
   ngOnInit() {
-    this.loginForm = this.formBuilder.group({
-      username: ['', Validators.required],
-      password: ['', Validators.required]
-    });
-
+    this.createForm();
     // reset login status
     this.authenticationService.logout();
-
     // get return url from route parameters or default to '/'
     this.returnUrl = this.route.snapshot.queryParams.returnUrl || '/';
   }
 
-  // convenience getter for easy access to form fields
-  get f() { return this.loginForm.controls; }
+  createForm() {
+    this.formGroup = this.formBuilder.group({
+      username: [null, [Validators.required]],
+      password: [null, [Validators.required]],
+      validate: ''
+    });
+  }
 
-  onSubmit() {
+  onSubmit(post) {
     this.submitted = true;
-
-    // stop here if form is invalid
-    if (this.loginForm.invalid) {
-      return;
-    }
-
-    this.loading = true;
-    this.authenticationService.login(this.f.username.value, this.f.password.value)
+    this.authenticationService.login(post.username, post.password)
       .pipe(first())
       .subscribe(
         data => {
@@ -57,8 +54,57 @@ export class LoginComponent implements OnInit {
         },
         error => {
           this.error = error;
-          this.loading = false;
         });
   }
-
 }
+  // loginForm: FormGroup;
+  // loading = false;
+  // submitted = false;
+  // returnUrl: string;
+  // error = '';
+  //
+  // constructor(
+  //   private formBuilder: FormBuilder,
+  //   private route: ActivatedRoute,
+  //   private router: Router,
+  //   private authenticationService: AuthenticationService
+  // ) { }
+  //
+  // ngOnInit() {
+  //   this.loginForm = this.formBuilder.group({
+  //     username: ['', Validators.required],
+  //     password: ['', Validators.required]
+  //   });
+  //
+  //   // reset login status
+  //   this.authenticationService.logout();
+  //
+  //   // get return url from route parameters or default to '/'
+  //   this.returnUrl = this.route.snapshot.queryParams.returnUrl || '/';
+  // }
+  //
+  // // convenience getter for easy access to form fields
+  // get f() { return this.loginForm.controls; }
+  //
+  // onSubmit() {
+  //   this.submitted = true;
+  //
+  //   // stop here if form is invalid
+  //   if (this.loginForm.invalid) {
+  //     return;
+  //   }
+  //
+  //   this.loading = true;
+  //   this.authenticationService.login(this.f.username.value, this.f.password.value)
+  //     .pipe(first())
+  //     .subscribe(
+  //       data => {
+  //         this.router.navigate([this.returnUrl]);
+  //       },
+  //       error => {
+  //         this.error = error;
+  //         this.loading = false;
+  //       });
+  // }
+
+//}
