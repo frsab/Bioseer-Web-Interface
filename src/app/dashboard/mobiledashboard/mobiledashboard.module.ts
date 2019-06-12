f420f00a35b1e7fbdd35f40e75dc0a4403338026
Travis CI <github@travis-ci.org>
2019-06-11 17:31:04 -0700

    Added mobile dashobard to its module

--- src/app/dashboard/mobiledashboard/mobiledashboard.module.ts
+++ src/app/dashboard/mobiledashboard/mobiledashboard.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MobiledashboardComponent} from './mobiledashboard.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MobiledashboardComponent
+  ],
   imports: [
     CommonModule
   ]
