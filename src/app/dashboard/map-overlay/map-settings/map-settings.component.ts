1b3269dec6119fcfca9bc24bd1d434e1476e5550
Travis CI <github@travis-ci.org>
2019-06-13 15:05:47 -0700

    Added event emitterts to map settings

--- src/app/dashboard/map-overlay/map-settings/map-settings.component.ts
+++ src/app/dashboard/map-overlay/map-settings/map-settings.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {observable, Observable, Subject} from 'rxjs';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-map-settings',
@@ -6,10 +8,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map-settings.component.scss']
 })
 export class MapSettingsComponent implements OnInit {
+  @Output() mapSettings: EventEmitter<mapSettings> = new EventEmitter();
+  @Output() newZone: EventEmitter<boolean> = new EventEmitter();
 
-  constructor() { }
+  mapSettingsForm: FormGroup;
 
-  ngOnInit() {
+  // TODO Make a form builder for this
+  constructor() {
+    this.mapSettingsForm = new FormGroup({
+      zoneView: new FormControl(1),
+      sensors: new FormControl(false),
+      mobileBuoys: new FormControl(false),
+      temperatureMap: new FormControl(false),
+      waterQualityMap: new FormControl(false),
+      phMap: new FormControl(false),
+      satellite: new FormControl(false),
+    });
+    this.onChanges()
   }
 
+  ngOnInit() {
+    this.mapSettingsForm.getRawValue();
+  }
+
+  onChanges(): void {
+    this.mapSettingsForm.valueChanges.subscribe(val => {
+      this.mapSettings.emit(val);
+    })
+  }
+
+  addZone() {
+    this.newZone.emit(true);
+  }
+
+}
+
+export class mapSettings {
+  zoneView: number;
+  sensors: boolean;
+  mobileBuoys: boolean;
+  temperatureMap: boolean;
+  waterQualityMap: boolean;
+  phMap: boolean;
+  satellite: boolean;
 }
